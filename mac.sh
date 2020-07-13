4a968d70017712536651577b2de6e8157e9701f4
Neo Chan <netkiller@msn.com>
2020-07-13 11:25:20 +0800

    更新

--- mac.sh
+++ mac.sh
@@ -1,4 +1,5 @@
 #rm -rf ~/tmp/management/
 mkdir -p ~/tmp/management/
 cp -r images ~/tmp/management/
+cp common/docbook.css ~/tmp/management/
 xsltproc -o ~/tmp/management/ docbook-xsl/docbook.mac.xsl book.xml
